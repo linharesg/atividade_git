35fe8dbf4a70aeb8339c7a5c01ae0dc3d1b6a105
Gabriel Linhares <linhares.gs96@gmail.com>
2023-09-25 10:43:16 -0300

    Estrutura da calculadora

---
+++ calculator.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+while True:
+    operacao = input("""
+Informe a a operação para efetuar o cálculo entre dois números:
+(+) Adição
+(-) Subtração
+(*) Multiplicação
+(/) Divisão
+""")
+
+    match operacao:
+        case "+":
+            pass
+
+
+
+        case "-":
+            pass
+
+
+
+        case "*":
+            pass
+
+
+
+        case "/":
+            pass
+
+
+
+        case _:
+            break
